b6e4b715300d357def82fd6150be02fe1b56b5ee
skang1004 <saejin.kang1004@gmail.com>
2020-06-09 17:41:45 -0700

    testing git clone

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const express = require('express');
-const passport = require('passport');
-const authRouter = require('./routes/auth.js');
-const fairpayController = require('./controllers/fairpayControllers');
-const cookieSession = require('cookie-session');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-require('./passport-setup');
+const path = require("path");
+const express = require("express");
+const passport = require("passport");
+const authRouter = require("./routes/auth.js");
+const fairpayController = require("./controllers/fairpayControllers");
+const cookieSession = require("cookie-session");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("./passport-setup");
 
 const app = express();
 const PORT = 3000;
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(
   cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
-    keys: ['wonderpus'],
+    keys: ["wonderpus"],
   })
 );
 
@@ -28,35 +28,35 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // route handlers
-app.use('/auth', authRouter);
+app.use("/auth", authRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/build', express.static(path.resolve(__dirname, '../build')));
-  app.get('/', (req, res) =>
-    res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
+if (process.env.NODE_ENV === "production") {
+  app.use("/build", express.static(path.resolve(__dirname, "../build")));
+  app.get("/", (req, res) =>
+    res.status(200).sendFile(path.resolve(__dirname, "../index.html"))
   );
 }
 
-app.get('/api/test', fairpayController.getUser, (req, res) => {
+app.get("/api/test", fairpayController.getUser, (req, res) => {
   res.status(200).json(res.locals.userData);
 });
 
 // Returns all user data
-app.get('/api/user', fairpayController.getUser, (req, res) => {
+app.get("/api/user", fairpayController.getUser, (req, res) => {
   res.status(200).json(res.locals.userData);
 });
 
 // Updates user with his/her personal, salary, and company information
 // If company does not exists in company table, it gets added
-app.post('/api/onboardUser', fairpayController.onboardUser, (req, res) => {
+app.post("/api/onboardUser", fairpayController.onboardUser, (req, res) => {
   //res.status(200).json(res.locals.userData);
-  res.status(200).redirect('http://localhost:3000/home');
+  res.status(200).redirect("http://localhost:3000/home");
 });
 
 // Returns a list of all job titles of users in the platform associated with
 // a particular company. Used for display a list for the user to select his/her
 // job title.
-app.post('/api/jobTitles', fairpayController.getCommonJobTitles, (req, res) => {
+app.post("/api/jobTitles", fairpayController.getCommonJobTitles, (req, res) => {
   res.status(200).json(res.locals.commonJobTitles);
 });
 
@@ -65,7 +65,7 @@ app.post('/api/jobTitles', fairpayController.getCommonJobTitles, (req, res) => {
 // });
 
 app.use(
-  '/api/company/:linkedin_user_id',
+  "/api/company/:linkedin_user_id",
   fairpayController.getCurrentUser,
   fairpayController.getCompanyData,
   fairpayController.getJobStats,
@@ -85,15 +85,15 @@ app.use(
 );
 
 // route error handler
-app.use('*', (req, res) => res.sendStatus(404));
+app.use("*", (req, res) => res.sendStatus(404));
 
 // global middleware error handler
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 400,
     message: {
-      err: 'An error occurred',
+      err: "An error occurred",
     },
   };
   const errorObj = Object.assign({}, defaultErr);
@@ -101,6 +101,6 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(PORT, () => console.log('Server started on port ', PORT));
+app.listen(PORT, () => console.log("Server started on port ", PORT));
 
 module.exports = app;
